e8515ae0246d1298142988a802f3cd7c23c19db9
dan <daniel.munozdev@gmail.com>
2017-11-22 20:29:44 -0800

    Add a specificFavoriteItems array and add a for loop to push variables into the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 //Business (or back-end) logic:
 let favoriteItems = [];
+let specificFavoriteItems = [];
 
 // Everything below this line is user interface (or front-end) logic
 $('form#form').submit(function(event) {
@@ -9,6 +10,12 @@ $('form#form').submit(function(event) {
     let favoriteItem = $('input#item' + [i]).val();
     favoriteItems.push(favoriteItem);
   }
-
+  // for loop to create specificFavoriteItems array
+  for (let i = 1; i < 4; i++) {
+    console.log('specific favorite item: ' + favoriteItems[i]);
+    specificFavoriteItems.push(favoriteItems[i]);
+  }
+  $('div.initially-hidden').slideDown();
   console.log(favoriteItems);
+  console.log(specificFavoriteItems);
 });
